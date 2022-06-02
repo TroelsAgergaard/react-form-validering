49809d9a26988d00da5f393335dcb38e9b46caff
TroelsAgergaard <troelsagergaard@gmail.com>
2022-06-02 15:09:20 +0200

    Add yup validerings  schema

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,19 +1,25 @@
 import { useForm } from "react-hook-form";
-// import Input from "./Input";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup
+  .object({
+    // firstName: yup.string().required(), default message...
+    firstName: yup.string().required("Venligst udfyld feltet..."),
+  })
+  .required();
 
 const Form = () => {
   const {
     register,
     formState: { errors },
     handleSubmit,
-  } = useForm();
+  } = useForm({ resolver: yupResolver(schema) });
   const onSubmit = (data) => console.log(data);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      {errors.firstName?.type === "required" && "First name is required!... "}
-      <br />
-      {/* <Input name="lastName" /> */}
+      {errors.firstName?.message}
       <input
         type="text"
         {...register("firstName", { required: true, maxLength: 10 })}
