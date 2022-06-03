1404ad8bdf05a392aa5eceb44686e5405a9fbc93
TroelsAgergaard <troelsagergaard@gmail.com>
2022-06-03 12:09:19 +0200

    Init commit

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,19 +1,36 @@
-import { useForm } from "react-hook-form";
-import Input from "./Input";
+import { useForm, Controller } from "react-hook-form";
+import Select from "react-select";
 
 const Form = () => {
   const {
     register,
     formState: { errors },
     handleSubmit,
+    control,
   } = useForm();
   const onSubmit = (data) => console.log(data);
+  const default_value = 1;
+  const options = [
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
+  ];
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      {errors.firstName?.type === "required" && "First name is required!... "}
-      <br />
-      <Input label="firstName" register={register} required />
+      <Controller
+        control={control}
+        defaultValue={default_value}
+        name="field_name_product"
+        render={({ onChange, value, name, ref }) => (
+          <Select
+            inputRef={ref}
+            options={options}
+            value={options.find((c) => c.value === value)}
+            onChange={(val) => onChange()}
+          />
+        )}
+      />
       <input type="submit" />
     </form>
   );
